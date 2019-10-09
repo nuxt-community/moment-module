89bb403de3c1cc9dac2a584333a71ce4086e775e
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2019-10-09 07:10:53 -0300

    chore: no need `babel`

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,5 @@
 module.exports = {
-  testEnvironment: 'node',
   collectCoverage: true,
-  collectCoverageFrom: [
-    'lib/**/*.js',
-    '!lib/plugin.js'
-  ],
-  moduleNameMapper: {
-    '^~/(.*)$': '<rootDir>/lib/$1',
-    '^~~$': '<rootDir>',
-    '^@@$': '<rootDir>',
-    '^@/(.*)$': '<rootDir>/lib/$1'
-  },
-  transform: {
-    '^.+\\.js$': 'babel-jest'
-  }
+  collectCoverageFrom: ['lib/**/*.js', '!lib/plugin.js'],
+  testEnvironment: 'node'
 }
